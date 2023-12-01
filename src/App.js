535db604f5681fde1dbd6a6a5762d2f99acc916b
kanebassirou <kaneb1927@gmail.com>
2023-12-01 21:16:13 +0000

    les formulaire avec reat sans controle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './assets/logo.png'
 // import Cart from './Cart'
 import ShoppingList from './components/ShoppingList'
 
+function handleSubmit(e) {
+    e.preventDefault()
+    alert(e.target['my_input'].value)
+}
+
+function QuestionForm () {
+	return <div>
+<form onSubmit={handleSubmit}>
+    <input type='text' name='my_input' defaultValue='Tapez votre texte' />
+    <button type='submit'>Entrer</button>
+</form>
+</div>
+}
+
 function App() {
 	return (
 		<div>
@@ -13,9 +27,13 @@ function App() {
 			{/* <Cart /> */}
 
 			<ShoppingList />
-			
-			
+			<QuestionForm />
+
+
+
 		</div>
+
+		
 	)
 }
 

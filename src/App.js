d7585b44177f326e00d7b10bf2e79b63293a54b1
kanebassirou <kaneb1927@gmail.com>
2023-12-01 22:45:02 +0000

    les formulaire avec reat qui sont controlé

--- src/App.js
+++ src/App.js
@@ -2,20 +2,7 @@ import Banner from './components/Banner'
 import logo from './assets/logo.png'
 // import Cart from './Cart'
 import ShoppingList from './components/ShoppingList'
-
-function handleSubmit(e) {
-    e.preventDefault()
-    alert(e.target['my_input'].value)
-}
-
-function QuestionForm () {
-	return <div>
-<form onSubmit={handleSubmit}>
-    <input type='text' name='my_input' defaultValue='Tapez votre texte' />
-    <button type='submit'>Entrer</button>
-</form>
-</div>
-}
+import QuestionForm from './components/QuestionForm'
 
 function App() {
 	return (
